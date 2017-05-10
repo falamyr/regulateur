48e47b9b581a229fc09e03e8c23b2df33fd1ae51
Folliet <pafolliet@hotmail.fr>
2017-05-10 02:15:09 +0200

    Intelligence.Decisions.decisionClasses : remove FOLD_CLASS

--- src/Intelligence/Decisions/decisionClasses.java
+++ src/Intelligence/Decisions/decisionClasses.java
@@ -5,21 +5,20 @@ public interface decisionClasses {
 	public static final int UNDEFINED_CLASS = -28;
 	public static final int CHECK_CLASS = 112;
 	public static final int CALL_CLASS = 113;
-	public static final int FOLD_CLASS = 114;
 	public static final int RAISE_CLASS = 115;
 	
 	
 	public static final int []NUMBER_CLASSES = 
 		{
+			1,
 			2,
-			3,
-			3
+			2
 		};
 	
 	public static final int [][]CLASSES = {
-		{CALL_CLASS, FOLD_CLASS},
-		{CHECK_CLASS, FOLD_CLASS, RAISE_CLASS},
-		{CALL_CLASS, FOLD_CLASS, RAISE_CLASS}
+		{CALL_CLASS},
+		{CHECK_CLASS,RAISE_CLASS},
+		{CALL_CLASS, RAISE_CLASS}
 	};
 
 }
