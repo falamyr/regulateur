13ebf17a11777fd4a81859c923f6d6f0fb88b0cb
falamyr <pafolliet@hotmail.fr>
2017-05-11 14:22:29 +0200

    Intelligence.Meteo.Meteo.java : modified toString() and classify() functions to implement total classification based on hands cards only

--- src/Intelligence/Meteo/meteoClasses.java
+++ src/Intelligence/Meteo/meteoClasses.java
@@ -5,13 +5,8 @@ public interface meteoClasses {
 	
 	public static final int NUMBER_CLASSES = 52*52;
 	
-	public static final int UNDEFINED_CLASS = -40;
-	public static final int LOW_CLASS = 52;
-	public static final int MEDIUM_CLASS = 53;
-	public static final int HIGH_CLASS = 54;
-	public static final int MIXED_CLASS = 55;
 	
-	public static final String []COLOR_STRINGS = {
+	public static final String []COLORS_STRINGS = {
 		"h",
 		"c",
 		"s",
