6a2bff239008beabeec978de2b68c5f7400e3461
falamyr <pafolliet@hotmail.fr>
2017-05-11 14:14:57 +0200

    Intelligence.Meteo.meteoClasses : adding color and value strings

--- src/Intelligence/Meteo/meteoClasses.java
+++ src/Intelligence/Meteo/meteoClasses.java
@@ -3,7 +3,7 @@ package Intelligence.Meteo;
 public interface meteoClasses {
 	
 	
-	public static final int NUMBER_CLASSES = 169;
+	public static final int NUMBER_CLASSES = 52*52;
 	
 	public static final int UNDEFINED_CLASS = -40;
 	public static final int LOW_CLASS = 52;
@@ -11,6 +11,28 @@ public interface meteoClasses {
 	public static final int HIGH_CLASS = 54;
 	public static final int MIXED_CLASS = 55;
 	
+	public static final String []COLOR_STRINGS = {
+		"h",
+		"c",
+		"s",
+		"d"
+	};
+	
+	public static final String []VALUES_STRINGS = {
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"T",
+		"J",
+		"Q",
+		"K",
+		"A"
+	};
 	
 	
 }
